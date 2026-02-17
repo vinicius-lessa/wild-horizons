59f589cdf08648caa543b034dbbc0bf220cbc207
Vinícius Lessa <vinicius.lessa33@outlook.com>
2026-02-17 18:40:00 -0300

    Add route not found to entrypoint

--- server.js
+++ server.js
@@ -1,22 +1,29 @@
-import http from 'node:http';
-import { getDataFromDb } from './database/database.js';
+import http from 'node:http'
+import { getDataFromDb } from './database/database.js'
 
 const hostname = '127.0.0.1'
 const port = 8000
 
 const server = http.createServer(async (request, response) => {
+  response.setHeader('Content-Type', 'application/json')
+
   const route = request.url
   const method = request.method
 
   if (route === '/api' && method === 'GET') {
     const destinations = await getDataFromDb()
-    response.setHeader('Content-Type', 'application/json')
-    response.statusCode = 200;
+    response.statusCode = 200
     response.write(JSON.stringify(destinations))
+ } else {
+    response.statusCode = 404
+    response.write(JSON.stringify({
+      error: "not found", 
+      message: "The requested route does not exist"
+    }))
   }
 
   response.end(() => {
-    console.log('Request is finished');
+    console.log('Request is finished')
   })
 })
 
