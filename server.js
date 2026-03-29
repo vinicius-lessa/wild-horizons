9c432a321c5dd025e67791375c10275250c4a89a
Vinícius Lessa <vinicius.lessa33@outlook.com>
2026-03-29 12:25:38 -0300

    feat: add new route `api/country`

--- server.js
+++ server.js
@@ -1,38 +1,47 @@
 import http from 'node:http'
 import { getDataFromDb } from './database/database.js'
-import { handleApiResponse } from './utils/utilities.js';
+import { handleApiResponse, filterDestinations } from './utils/utilities.js';
 
 const hostname = '127.0.0.1'
 const port = 8000
 
-const server = http.createServer(async (request, response) => {
-  response.setHeader('Content-Type', 'application/json')
+const server = http.createServer(async (req, res) => {
+  res.setHeader('Content-Type', 'application/json')
 
-  const route = request.url
-  const method = request.method
+  const route = req.url
+  const method = req.method
 
   const destinations = await getDataFromDb()
 
+  // /api -----------------------------------------------------------------
   if (route === '/api' && method === 'GET') {
-    handleApiResponse(response, 200, destinations)
+    handleApiResponse(res, 200, destinations)
   }
-  else if (request.url.startsWith('/api/continent') && method === 'GET') {
-    // Get last part
-    const targetContinent = request.url.split('/').pop()
 
-    // Filter Asian Destinations
-    const filteredDestinations = destinations.filter((destination) => {
-      return destination.continent.toLowerCase() === targetContinent.toLowerCase()
-    })
-    
-    handleApiResponse(response, 200, filteredDestinations)
+  // /api/continent/<continent> -------------------------------------------
+  else if (req.url.startsWith('/api/continent/') && method === 'GET') {
+    const requestedContinent = req.url.split('/').pop() // Get last part
 
-  } else {
-    handleApiResponse(response, 404, {
+    // Filter destinations by continent
+    const filteredDestinations = filterDestinations(destinations, 'continent', requestedContinent);
+    handleApiResponse(res, 200, filteredDestinations)
+  } 
+  
+  // /api/country/<country> ----------------------------------------------
+  else if (req.url.startsWith('/api/country/') && method === 'GET') {
+    const requestedCoutry = req.url.split('/').pop()
+
+    // Filter destinations by country
+    const filteredDestinations = filterDestinations(destinations, 'country', requestedCoutry)
+    handleApiResponse(res, 200, filteredDestinations)
+  }
+  
+  else {
+    handleApiResponse(res, 404, {
       error: "not found", 
-      message: "The requested route does not exist"
+      message: "The requested route does not aexist"
     })
-  }  
+  }
 })
 
 
