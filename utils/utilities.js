d3daceaf611a5dc54df80e28361dcb54ace08a15
Vinícius Lessa <vinicius.lessa33@outlook.com>
2026-03-29 12:26:46 -0300

    refactor: DRY practice with utils script

--- utils/utilities.js
+++ utils/utilities.js
@@ -1,7 +1,13 @@
-export const handleApiResponse = (response, statusCode, data) => {
-  response.statusCode = statusCode
-  response.write(JSON.stringify(data))
-  response.end(() => {
+export const handleApiResponse = (res, statusCode, data) => {
+  res.statusCode = statusCode
+  res.write(JSON.stringify(data))
+  res.end(() => {
     console.log('Request is finished')
   })
+}
+
+export const filterDestinations = (destinations, locationType, locationName) => {
+  return destinations.filter((destination) => {
+      return destination[locationType].toLowerCase() === locationName.toLowerCase()
+    })
 }
